9dca49f6096889356f04f752fc51d9ad06823d9e
hezhiren <hezhiren1994@163.com>
2020-07-21 16:22:45 +0800

    TabBar组件实战

---
+++ tabBar/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="tab-bar-item" @click="itemClick">
+      <div v-if="!isActive"><slot name='item-icon'></slot></div>
+      <div v-else><slot name='item-icon-active'></slot></div>
+      <div :style="activeStyle" ><slot name='item-text'></slot></div>
+     
+      <!-- <img src="../../assets/img/tabbar/home.svg" alt="">
+      <div>首页</div> -->
+    </div>
+    <!-- <div class="tab-bar-item">
+       <img src="../../assets/img/tabbar/home.svg" alt="">
+       <div>分类</div>
+    </div>
+    <div class="tab-bar-item">
+       <img src="../../assets/img/tabbar/home.svg" alt="">
+       <div>购物车</div>
+    </div>
+    <div class="tab-bar-item">
+       <img src="../../assets/img/tabbar/home.svg" alt="">
+       <div>我的</div> -->
+    <!-- </div> -->
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  props:{
+    path: String,
+    activeColor: {
+      type: String,
+      default: '#FF5777'
+    }
+  },
+  data() {
+    return {
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    // 文字动态颜色控制
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style>
+ .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+    margin-top: 3px;
+  }
+
+  /* .active {
+    color:#FF5777;
+  } */
+</style>
